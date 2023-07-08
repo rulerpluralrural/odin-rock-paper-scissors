3ca7969124993cc57b3af4129178b4443bf25e94
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-07-08 15:37:48 +0800

    "Added restart button and functionality "
    * restart button will appear once either the player or computer wins and clicking will trigger refreshPage() function which will reload the page.
    
    * added stopGame() function which will stop the game once the player or the computer wins.
    
    * removed gameStart() function and added pickChoice function
    that will get the data attribute of the button clicked;

--- app.js
+++ app.js
@@ -12,21 +12,29 @@ const computerPick = document.getElementById('computer-pick');
 const pickRock = document.getElementById('btn-rock');
 const pickPaper = document.getElementById('btn-paper');
 const pickScissors = document.getElementById('btn-scissors');
-
 const choices = {
     rock: 'Images/rock.png',
     paper: 'Images/paper.png',
     scissors: 'Images/scissors.png'
 };
 
-// Computer choice function //
-function getComputerChoice () {
-    const choices = ['rock', 'paper', 'scissors'];
-    const randomNum = Math.floor(Math.random() * 3);
+// restart btn variable
+const restart = document.querySelector('.refresh-btn');
 
-    return choices[randomNum];
+// restart game function
+function refreshPage() {
+    location.reload();
 }
 
+// remove click event function 
+function stopGame() {
+    pickRock.removeEventListener("click", pickChoice, true);
+    pickPaper.removeEventListener("click", pickChoice, true);
+    pickScissors.removeEventListener("click", pickChoice, true);
+}
+
+
+// player health bar dmg function
 function playerWon() {
     const remainingScore = document.querySelectorAll('.circle-comp');
     remainingScore[remainingScore.length - 1].className = 'circle-dmg';
@@ -35,6 +43,9 @@ function playerWon() {
     if(remainingScore.length == 1) {
         winner.className = 'wresult';
         winner.textContent = 'Winner : Player Won!';
+        restart.classList.remove('hidden');
+        // call remove click event if health is empty
+        stopGame();
     }
 }
 
@@ -46,6 +57,9 @@ function computerWon() {
     if(remainingScore.length == 1) {
         winner.className = 'lresult';
         winner.textContent = 'Winner : Computer Won!';
+        restart.classList.remove('hidden');
+        // call remove click event if health is empty
+        stopGame();
     }
 }
 
@@ -54,12 +68,22 @@ function draw() {
     actionText.className = 'draw';
 }
 
+// Computer random choice function 
+function getComputerChoice () {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * 3);
+
+    return choices[randomNum];
+}
 
 // Choices comparison function //
 function game(playerChoice) {
+    // pass in computer choice
     const computerChoice = getComputerChoice();
+    // set img src to whatever computer picks
     computerPick.setAttribute('src', choices[computerChoice]);
-    setInterval(computerChoice, 500);
+    //set img src tp whatever player picks
+    playerPick.src = choices[playerChoice];
 
     actionText.textContent = 'Pick your choice!';
     actionText.className = 'default';
@@ -91,23 +115,14 @@ function game(playerChoice) {
 //(scissors - paper = win), 
 //(rock - scissors = win).
 
-
-// Button click function //
-function gameStart() {
-    pickRock.addEventListener('click', function() {
-        playerPick.src = 'Images/rock.png';
-        game('rock');
-    });
-
-    pickPaper.addEventListener('click', function() {
-        playerPick.src = 'Images/paper.png';
-        game('paper');
-    });
-
-    pickScissors.addEventListener('click', function() {
-        playerPick.src = 'Images/scissors.png';
-        game('scissors');
-    });
+// pull data attribute from the buttons function
+function pickChoice() {
+    const pick = this.getAttribute('data-type');
+    game(pick)
 }
 
-gameStart();
+// Button click event listener
+    pickRock.addEventListener('click', pickChoice, true);
+    pickPaper.addEventListener('click', pickChoice, true);
+    pickScissors.addEventListener('click', pickChoice, true);
+    restart.addEventListener('click', refreshPage);
